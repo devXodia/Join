d981c761658648f5bafcce3944b4d3956b03d903
Alen Alduk <youstylez@gmail.com>
2023-09-30 14:54:00 +0200

    fixed scroll on mobile

--- js/board_edit_templates.js
+++ js/board_edit_templates.js
@@ -1,5 +1,5 @@
-function htmlEditOverview(id, colorCode, prio){
-    return /*html*/`
+function htmlEditOverview(id, colorCode, prio) {
+  return /*html*/ `
     <div id="confirmDeleteTask" class="d-none">
     </div>
     
@@ -7,10 +7,7 @@ function htmlEditOverview(id, colorCode, prio){
     <div  id="wrapper_for_close" style="position: absolute; width: 100%; height: 100%;"></div>
         <div id="editTaskContainerClose" onclick="saveBoard(${id})"><img src="/assets/img/Icon_close.png" alt="">
         </div>
-        <div id="editTaskContainerEditDelete">
-            <div id="editTaskContainerDelete"></div>
-            <div id="editTaskContainerEdit" onclick="openEditMode(${id})"><img src="/assets/img/Icon_edit.png"></div>
-        </div>
+        <!-- Here was edittask  buttons before -->
         <div id="editTaskContainerInner">
             
             <div id="editTaskContainerCategory" style="background-color: ${colorCode}"></div>
@@ -32,6 +29,10 @@ function htmlEditOverview(id, colorCode, prio){
             <div id=editTaskContainerSubtasksText>Subtasks</div>
             <div id=editTaskContainerSubtasksTasks></div>
             </div>
+            <div id="editTaskContainerEditDelete">
+            <div id="editTaskContainerDelete"></div>
+            <div id="editTaskContainerEdit" onclick="openEditMode(${id})"><img src="/assets/img/Icon_edit.png"></div>
+        </div>
         </div>
     
     </div>
@@ -39,8 +40,8 @@ function htmlEditOverview(id, colorCode, prio){
 `;
 }
 
-function editModeTemplate(id, task){
-    return /*html*/`
+function editModeTemplate(id, task) {
+  return /*html*/ `
     <form id="editTaskContainer" onsubmit="saveEditedBoard(${id}); return false;">
         <div id="editTaskContainerClose" onclick="closeEditTask()"><img src="assets/img/Icon_close.png" alt="">
         </div>
@@ -51,11 +52,11 @@ function editModeTemplate(id, task){
         <div id="editTaskContainerInner" class="editContainerInner" onclick="closeOptionsOnClick(event, 'Edit')">
             <div id="editTaskTitle" class="editTaskTitleFixed editTasksWidth80">
                 <div id="editTaskTitleFixed">Title</div>
-                <input id="editTaskTitleChangable" required class="inputsAddTask" value="${task['title']}" maxlength="30">
+                <input id="editTaskTitleChangable" required class="inputsAddTask" value="${task["title"]}" maxlength="30">
             </div>
             <div id="editTaskDescription" class="editTaskTitleFixed editTasksWidth80">
                 <div id="editTaskDescriptionFixed">Description</div>
-                <textarea id="editTaskDescriptionChangable" class="inputsAddTask" required maxlength="100">${task['description']}</textarea>
+                <textarea id="editTaskDescriptionChangable" class="inputsAddTask" required maxlength="100">${task["description"]}</textarea>
             </div>
             <div id="editTaskDueDate" class="editTaskTitleFixed editTasksWidth80">
                 <div id="editTaskDueDateFixed">Due Date</div>
@@ -64,7 +65,7 @@ function editModeTemplate(id, task){
                         class="inputsAddTask height51 padding hover"
                         type="date"
                         required
-                        value="${task['dueDate']}"       
+                        value="${task["dueDate"]}"       
                         />
             </div> 
             <div id="editTaskPrio" class="editTaskTitleFixed editTasksWidth80">
@@ -90,5 +91,5 @@ function editModeTemplate(id, task){
             </div>
         </div>
 </form>
-`
+`;
 }
